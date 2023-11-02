1d6c7297c6631c243a24873543c61167988fc8dd
Valerii <valeriykundas@gmail.com>
2023-11-03 01:10:31 +0200

    TestUploadUserAvatar

--- handlers_test.go
+++ handlers_test.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/utils"
 	"gorm.io/gorm"
 )
@@ -31,4 +36,49 @@ func TestGetUsers(t *testing.T) {
 	utils.AssertEqual(t, len(users), len(data.Users))
 	utils.AssertEqual(t, users[0].Name, data.Users[0].Name)
 	utils.AssertEqual(t, users[5].Name, data.Users[5].Name)
+}
+
+func TestUploadUserAvatar(t *testing.T) {
+	var clearDB func(*gorm.DB) error
+	DB, clearDB = prepareTestDb(t)
+	defer clearDB(DB)
+	app := createApp(DB)
+
+	user, err := addRandomUser(DB)
+	utils.AssertEqual(t, nil, err)
+
+	fileName := "test.jpeg"
+	file, err := os.Open(fileName)
+	utils.AssertEqual(t, nil, err)
+
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	formData, err := writer.CreateFormFile("image", fileName)
+	utils.AssertEqual(t, nil, err)
+
+	_, err = io.Copy(formData, file)
+	utils.AssertEqual(t, nil, err)
+
+	writer.Close()
+
+	req := httptest.NewRequest(fiber.MethodPost, fmt.Sprintf("/api/users/%d/avatar", user.ID), body)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	resp, err := app.Test(req)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
+
+	bytes, err := io.ReadAll(resp.Body)
+	utils.AssertEqual(t, nil, err)
+
+	var data map[string]any
+	err = json.Unmarshal(bytes, &data)
+	utils.AssertEqual(t, nil, err)
+
+	var resultUser User
+	tx := DB.First(&resultUser, user.ID)
+	utils.AssertEqual(t, nil, tx.Error)
+
+	// TODO: currently saves into the same repo as prod `uploads`, would be better to make a temporary repo
+	utils.AssertEqual(t, fileName, resultUser.AvatarFileName)
 }
