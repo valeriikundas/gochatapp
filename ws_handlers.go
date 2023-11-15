586b5f2dbecf707f3161f004d7ce14b2fb42e01c
Valerii <valeriykundas@gmail.com>
2023-11-15 18:33:35 +0200

    ref: extract function

--- ws_handlers.go
+++ ws_handlers.go
@@ -54,89 +54,101 @@ func WebsocketHandler(c *websocket.Conn) {
 
 		switch messageType := v.Type; messageType {
 		case "join_chat":
-			var requestData JoinChatRequestSchema
-			err = json.Unmarshal(message, &requestData)
-			if err != nil {
-				log.Fatalf("json unmarshall JoinChatRequestSchema error: %s\n", err)
-			}
-			log.Debugf("`join chat` message=%+v\n", requestData)
-
-			userID := requestData.UserID
-			_, exists := websocketConnections[userID]
-			if exists {
-				websocketConnections[userID].Close()
-			}
-			websocketConnections[userID] = c
-
-			// TODO: broadcast to other users in chat, than a new user has joined
+			handleJoinChat(c, message)
 
 		case "send_message":
-			var requestData SendMessageRequestSchema
-			err = json.Unmarshal(message, &requestData)
-			if err != nil {
-				log.Fatalf("json unmarshall SendMessageRequestSchema error: %s\n", err)
-			}
-			log.Debugf("`send message` message=%+v\n", requestData)
-
-			messageContent := string(requestData.Message)
-			log.Debugf("messageContent=%s\n", messageContent)
-			messageObj := Message{
-				ChatID:  requestData.ChatID,
-				FromID:  requestData.UserID,
-				Content: messageContent,
-			}
-			tx := DB.Create(&messageObj)
-			if tx.Error != nil {
-				log.Fatal(tx.Error)
-			}
-
-			userIDsToSendMessageTo, err := getChatUsersExcept(requestData.ChatID, requestData.UserID)
-			if err != nil {
-				log.Fatalf("getChatUsersExcept err=%s\n", err)
-			}
-			log.Debugf("will send message to userIDsToSendMessageTo=%+v\n", userIDsToSendMessageTo)
-
 			// get users that are in chat and not userID
-			for _, userID := range userIDsToSendMessageTo {
-				memberConn := websocketConnections[userID]
-
-				if memberConn == nil {
-					log.Debugf("no connection for userID=%d\n", userID)
-					continue
-				}
-
-				twentySecondFromNow := time.Now().Add(time.Second * 10)
-				err = memberConn.WriteControl(websocket.PingMessage, []byte("hello from the other side"), twentySecondFromNow)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				var fromUser User
-				err = DB.Find(&fromUser, userID).Error
-				if err != nil {
-					log.Fatalf("get user by id failed id=%d\n", requestData.UserID)
-				}
-
-				broadcastMessageData := BroadcastMessageSchema{
-					BaseMessageSchema: BaseMessageSchema{
-						Type: "new_message",
-					},
-					FromUserEmail: fromUser.Email,
-					Message:       messageContent,
-				}
-				b, err := json.Marshal(broadcastMessageData)
-				if err != nil {
-					log.Fatalf("json marshall err:%s\n", err)
-				}
-
-				err = memberConn.WriteMessage(websocket.TextMessage, b)
-				if err != nil {
-					log.Fatalf("error WriteMessage %s\n", err)
-				}
-			}
+			handleSendMessage(message)
 
 		default:
 			log.Errorf("unhandled message type=%s v=%s\n", messageType, v)
 		}
 	}
+}
+
+func handleSendMessage(message []byte) {
+	var requestData SendMessageRequestSchema
+	err := json.Unmarshal(message, &requestData)
+	if err != nil {
+		log.Fatalf("json unmarshall SendMessageRequestSchema error: %s\n", err)
+	}
+	log.Debugf("`send message` message=%+v\n", requestData)
+
+	messageContent := string(requestData.Message)
+	log.Debugf("messageContent=%s\n", messageContent)
+	messageObj := Message{
+		ChatID:  requestData.ChatID,
+		FromID:  requestData.UserID,
+		Content: messageContent,
+	}
+	tx := DB.Create(&messageObj)
+	if tx.Error != nil {
+		log.Fatal(tx.Error)
+	}
+
+	userIDsToSendMessageTo, err := getChatUsersExcept(requestData.ChatID, requestData.UserID)
+	if err != nil {
+		log.Fatalf("getChatUsersExcept err=%s\n", err)
+	}
+	log.Debugf("will send message to userIDsToSendMessageTo=%+v\n", userIDsToSendMessageTo)
+
+	for _, userID := range userIDsToSendMessageTo {
+		sendMessageToUser(userID, messageContent)
+	}
+}
+
+func sendMessageToUser(userID uint, messageContent string) {
+	memberConn := websocketConnections[userID]
+
+	if memberConn == nil {
+		log.Debugf("no connection for userID=%d\n", userID)
+		return
+	}
+
+	twentySecondFromNow := time.Now().Add(time.Second * 10)
+	err := memberConn.WriteControl(websocket.PingMessage, []byte("hello from the other side"), twentySecondFromNow)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var fromUser User
+	err = DB.Find(&fromUser, userID).Error
+	if err != nil {
+		log.Fatalf("get user by id failed id=%d\n", userID)
+	}
+
+	broadcastMessageData := BroadcastMessageSchema{
+		BaseMessageSchema: BaseMessageSchema{
+			Type: "new_message",
+		},
+		FromUserEmail: fromUser.Email,
+		Message:       messageContent,
+	}
+	b, err := json.Marshal(broadcastMessageData)
+	if err != nil {
+		log.Fatalf("json marshall err:%s\n", err)
+	}
+
+	err = memberConn.WriteMessage(websocket.TextMessage, b)
+	if err != nil {
+		log.Fatalf("error WriteMessage %s\n", err)
+	}
+}
+
+func handleJoinChat(c *websocket.Conn, message []byte) {
+	var requestData JoinChatRequestSchema
+	err := json.Unmarshal(message, &requestData)
+	if err != nil {
+		log.Fatalf("json unmarshall JoinChatRequestSchema error: %s\n", err)
+	}
+	log.Debugf("`join chat` message=%+v\n", requestData)
+
+	userID := requestData.UserID
+	_, exists := websocketConnections[userID]
+	if exists {
+		websocketConnections[userID].Close()
+	}
+	websocketConnections[userID] = c
+
+	// TODO: broadcast to other users in chat, than a new user has joined
 }
